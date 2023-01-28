3d234872e6f10511a6c416e57fa6a292f3c31009
Pipetboy <felipefelizx@gmail.com>
2023-01-28 02:50:53 -0300

    Fix: Key duplicates

--- src/Components/Team.jsx
+++ src/Components/Team.jsx
@@ -1,34 +1,48 @@
-import React from 'react'
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
 
 export const Team = (props) => {
-    const { photos } = props;
-    const names = ["Felipe", "Barbara", "Juan", "Maria", "Christopher", "Marcelo"];
-    const job = ["Product owner","Art Diector" ,"Teach lead" ,"UX Designer","Developer","Developer" ];
+  const { photos } = props;
+  const names = [
+    "Felipe",
+    "Barbara",
+    "Juan",
+    "Maria",
+    "Christopher",
+    "Marcelo",
+  ];
+  const job = [
+    "Product owner",
+    "Art Diector",
+    "Teach lead",
+    "UX Designer",
+    "Developer",
+    "Developer",
+  ];
 
-
-    return (
-      <div>
-        <div className="container">
-          {photos.map((photo, index) => (
-            <div className="card">
-              <div className="card-foto-job">
-                <img src={photos[index]} alt="photo" />
-                <p className="job">
-                  {job[index].split("").map((char) => (
-                    <span
-                      className="job-char"
-                      style={{ writingMode: "vertical-rl" }}
-                    >
-                      {char}
-                    </span>
-                  ))}
-                </p>
-              </div>
-              <p className="name">{names[index]}</p>
+  return (
+    <div>
+      <div className="container">
+        {photos.map((photo, index) => (
+          <div className="card" key={uuidv4()}>
+            <div className="card-foto-job">
+              <img src={photo} alt="photo" />
+              <p className="job">
+                {job[index].split("").map((char, charIndex) => (
+                  <span
+                    className="job-char"
+                    style={{ writingMode: "vertical-rl" }}
+                    key={`${index}-${charIndex}`}
+                  >
+                    {char}
+                  </span>
+                ))}
+              </p>
             </div>
-          ))}
-        </div>
+            <p className="name">{names[index]}</p>
+          </div>
+        ))}
       </div>
-    );
-}
-
+    </div>
+  );
+};
